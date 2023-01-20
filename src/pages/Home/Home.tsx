39d5855609fc392738d23c6e024290977df34e94
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-01-20 13:08:35 -0300

    feat: search

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,38 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../../components/molecules/NavBar/NavBar";
 import ProductItem from "../../components/molecules/ProductItem/ProductItem";
-import Seek from "../../components/molecules/Seek/Seek";
 import { api } from "../../utils/api/api";
 import { BoxHome } from "./style";
 
 export type game = {
   id: string;
-  title: string;
+  Title: string;
   CoverImageUrl: string;
 };
 
 const Home = () => {
-  const [seek, setSeek] = useState("");
+
 
   const [games, setGames] = useState<game[]>([]);
+  const [search, setSearch] = useState("");
 
   async function Games() {
     const games = await api.getGames();
     setGames(games);
   }
 
+  const sortedGames =
+    search.length > 0
+      ? games.filter((game) =>
+          game.Title.toLowerCase().includes(search.toLowerCase())
+        )
+      : games;
+
   useEffect(() => {
     Games();
   }, []);
 
-  console.log(games);
+  console.log("renderizou");
 
   return (
     <>
@@ -34,11 +41,18 @@ const Home = () => {
       </header>
       <main>
         <BoxHome>
-          <Seek seek={seek} setSeek={setSeek} />
-        
-          {games.map((game) => (
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearch(e.currentTarget.value);
+            }}
+            placeholder="Search"
+          />
+
+          {sortedGames.map((game) => (
             <ProductItem key={game.id} game={game} />
           ))}
+
         </BoxHome>
       </main>
     </>
